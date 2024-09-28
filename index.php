7483be7b59fa47f3126d78492caaa13fe8c46b41
yansuogaming <80459529+yansuogaming@users.noreply.github.com>
2024-09-28 10:21:28 +0700

    Updated

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB ONLINE</title>
+
+    <!-- Link CSS Bootstrap  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select/dist/css/bootstrap-select.min.css">
+
+    <!-- Link Font-Awesome -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fa6/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flag-icons/css/flag-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/owl.carousel/dist/assets/owl.carousel.min.css">
+
+    <!-- Local CSS -->
+    <link rel="stylesheet" href="source/css/header.css">
+    <link rel="stylesheet" href="fontawesome-pro-6.6.0-web/css/all.css">
+
+   
+</head>
+
+<body>
+     <!-- Link JS  -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/owl.carousel/dist/owl.carousel.min.js"></script>
+
+    <!-- Local JS  -->
+    <!-- <script src="script.js"></script> -->
+
+    <?php include 'html/header.html'; ?>
+    <?php include 'html/home.html'; ?>
+    <?php include 'html/footer.html'; ?>
+
+    
+</body>
+
+</html>
